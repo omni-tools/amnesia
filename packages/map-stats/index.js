b40b711bae1b711918c4e1c9d0da8c71d32fa308
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-11 00:07:00 +0200

    Try out to collect detailed stats by key :bookmark:

--- packages/map-stats/index.js
+++ packages/map-stats/index.js
@@ -1,4 +1,9 @@
 'use strict';
+
+const increment = (object, key) => {
+	object[key] = (object[key] || 0) + 1;
+}
+
 class StatsMap extends Map {
 
 	constructor(iterable) {
@@ -8,38 +13,44 @@ class StatsMap extends Map {
 			hits: 0,
 			misses: 0
 		};
-	}
+		this._detailedStats = { // ByKey
+			read: {},
+			write: {},
+			miss: {},
+			check: {}
+		};
+	};
 
 	get stats() {
 		return this._stats;
 	}
 
+	get detailedStats() {
+		return this._detailedStats
+	}
+
 	set(key, value) {
-		const data = value.data;
-
-		Object.defineProperty(value, 'data', {
-			get: () => {
-				this._stats.misses--;
-				this._stats.hits++;
-				return data;
-			}
-		});
-
-		super.set(key, value);
+		increment(this._detailedStats.write, key);
+		return super.set(key, value);
 	}
 
 	get(key) {
-		this._stats.misses++;
+		increment(this._detailedStats.read, key);
+		if (super.has(key)) {
+			this._stats.hits++;
+		} else {
+			increment(this._detailedStats.miss, key);
+			this._stats.misses++;
+		}
 		return super.get(key);
 	}
 
 	has(key) {
 		const has = super.has(key);
-
+		increment(this._detailedStats.check, key);
 		if (!has) {
 			this._stats.misses++;
 		}
-
 		return has;
 	}
 }
--- packages/map-stats/test.js
+++ packages/map-stats/test.js
@@ -12,27 +12,43 @@ test('initial stats', t => {
 
 test('stats', t => {
 	const map = new StatsMap();
-	map.set('foo', {data: 'bar'});
+	map.set('foo', 'bar');
 
-	const ret = map.get('foo');
-
-	t.deepEqual(map.stats, {
-		hits: 0,
-		misses: 1
-	});
-
-	t.deepEqual(ret.data, 'bar');
-
+	t.deepEqual(map.get('foo'), 'bar');
 	t.deepEqual(map.stats, {
 		hits: 1,
 		misses: 0
 	});
-
-	map.has('foo');
-	map.has('unicorn');
+	t.is(map.get('lost'), undefined);
 
 	t.deepEqual(map.stats, {
 		hits: 1,
 		misses: 1
+	});
+
+	map.has('unicorn'); // will incread missed
+	map.has('foo'); // but not hits
+
+	map.has('unicorn'); // and one more
+
+	t.deepEqual(map.stats, {
+		hits: 1,
+		misses: 3
+	});
+	t.deepEqual(map.detailedStats, {
+		read: {
+			foo: 1,
+			lost: 1
+		},
+		miss: {
+			lost: 1
+		},
+		write: {
+			foo: 1
+		},
+		check: {
+			foo: 1,
+			unicorn: 2
+		}
 	});
 });
